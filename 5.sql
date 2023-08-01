f1793aa647f24895bae95b60fe9b5ce45f2eec9e
caron14 <supika.caron@gmail.com>
2023-08-01 21:45:00 +0900

    AC

---
+++ 5.sql
@@ -0,0 +1,25 @@
+-- Input: 
+-- Tweets table:
+-- +----------+----------------------------------+
+-- | tweet_id | content                          |
+-- +----------+----------------------------------+
+-- | 1        | Vote for Biden                   |
+-- | 2        | Let us make America great again! |
+-- +----------+----------------------------------+
+-- Output: 
+-- +----------+
+-- | tweet_id |
+-- +----------+
+-- | 2        |
+-- +----------+
+-- Explanation: 
+-- Tweet 1 has length = 14. It is a valid tweet.
+-- Tweet 2 has length = 32. It is an invalid tweet.
+
+select 
+  tweet_id
+from
+  Tweets
+where
+  length(content) > 15
+;
